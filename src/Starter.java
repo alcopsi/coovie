e4b38786e3f00853fe265df7c92853880e0b59e2
deshi-basara <simon@i-was-perfect.net>
2012-08-10 18:40:11 +0200

    Luder comment

--- src/Starter.java
+++ src/Starter.java
@@ -9,7 +9,7 @@ public class Starter {
 		InputPath i = new InputPath(); 
 		String p = i.getPath(); // get ScanPath
 		Scanner scan = new Scanner(p); // scans ScanPath for movies
-		scan.scanPath();
+		scan.scanPath(); // Luder 
 		scan.showResult();
 	}
 }
